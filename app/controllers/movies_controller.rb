3c6f36c6975756786feb367e682f7dae030355cd
vagrant <vagrant@localhost.localdomain>
2020-07-18 15:14:30 +0000

    controller fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,24 @@
 class MoviesController < ApplicationController
   def index
-  @movies = Movie.all
+  	 @movies = Movie.all
   end
 
   def show
+  	 @movie = Movie.find(params[:id])
+  	 @user = @movie.user
+
   end
 
   def edit
+  @book = Book.find(params[:id])
   end
 
   def new
   end
+
+  private
+  def movie_params
+      params.require(:movie).permit(:title, :impression)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  get 'search/search'
-  get 'relationships/follow'
-  get 'relationships/followed'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/confirm'
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/edit'
-  get 'movies/new'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users,only: [:show,:edit,:update,:confirm] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'home#top'
+resources :movies
+resource :favorites, only: [:create, :destroy]
+resource :movie_comments, only: [:create, :destroy]
 end
