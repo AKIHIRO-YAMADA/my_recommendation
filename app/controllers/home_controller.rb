e990f688f45f1afbc16b204295d0f5d75f97b348
vagrant <vagrant@localhost.localdomain>
2020-07-28 17:20:24 +0000

    view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def top
-    @movies = Movie.all
+	@movies = Movie.page(params[:page]).per(9)
+	if params[:category_id]
+	  @movies = Movie.where(category_id: params[:category_id]).page(params[:page]).per(9)
+	else
+	  @movies = Movie.joins(:category).page(params[:page]).per(9)
+	end
   end
+
   def about
   end
+
 end
